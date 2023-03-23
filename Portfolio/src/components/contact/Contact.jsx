b63e48b093293ba5d52d250a7a8a9b478408ce8d
Lucky yadav <luckyyadav8627@gmail.com>
2023-03-23 22:55:41 +0530

    Update Cantacts and Portfolio CSS

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -32,34 +32,46 @@ const Contact = () => {
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
-        <div className="contact__options">
-          <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
-            <h4>Email</h4>
-            <h5>luckyyadav8627@gmail.com</h5>
-            <a href="mailto:luckyyadav8627@gmail.com">Send a message</a>
-          </article>
-          <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
-            <h4>WhatsApp</h4>
-            <h5>+91-9485949413</h5>
-            <a href="https://api.whatspp.com/send?phone=+919485949413ss">
-              Send a message
-            </a>
-          </article>
-          <article className="contact__option">
-            <img
-              src={Linkedinicon}
-              alt="Loading..."
-              className="contact__option-icon linkedin_icon"
-            />
-            <h4>Linkedin</h4>
-            <h5>Lucky-yadav</h5>
-            <a href="https://www.linkedin.com/in/lucky-yadav/">
-              Send a message
-            </a>
-          </article>
+        <div className="lefta">
+          <div className="pyramid">
+            <div className="contact__options">
+              <div className="glow"></div>
+              <span style={{ "--i": "0" }} className="contact__option">
+                <MdOutlineEmail className="contact__option-icon" />
+                <h4>Gmail</h4>
+                <h5>luckyyadav8627</h5>
+                <a href="mailto:luckyyadav8627@gmail.com">Let's Connect</a>
+              </span>
+              <span style={{ "--i": "1" }} className="contact__option">
+                <MdOutlineEmail className="contact__option-icon" />
+                <h4>WhatsApp</h4>
+                <h5>+91-9485949413</h5>
+                <a href="https://api.whatspp.com/send?phone=+919485949413ss">
+                  Let's Connect
+                </a>
+              </span>
+              <span style={{ "--i": "2" }} className="contact__option">
+                <img
+                  src={Linkedinicon}
+                  alt="Loading..."
+                  className="contact__option-icon linkedin_icon"
+                />
+                <h4>Linkedin</h4>
+                <h5>Lucky-yadav</h5>
+                <a href="https://www.linkedin.com/in/lucky-yadav/">
+                  Let's Connect
+                </a>
+              </span>
+              <span style={{ "--i": "3" }} className="contact__option">
+                <MdOutlineEmail className="contact__option-icon" />
+                <h4>Github</h4>
+                <h5>Lucky-Yadav</h5>
+                <a href="https://github.com/Lucky-Yadav">Let's Connect</a>
+              </span>
+            </div>
+          </div>
         </div>
+
         <form ref={formRef} onSubmit={handleSubmit}>
           <input
             type="text"
